fc22e04b18390700cfb37115aaee3e48da796cb1
yuebin.shen <yuebin.shen@alipay.com>
2018-10-31 19:41:15 +0800

    remove mvc

--- df-test/src/main/java/com/brotherb/df/App.java
+++ df-test/src/main/java/com/brotherb/df/App.java
@@ -1,12 +1,15 @@
 package com.brotherb.df;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 /**
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("spring/spring-context.xml");
+
     }
 }
